144966b35631181fc378760b6bc65eeda8d55a77
Miguel Angel Sanchez Mares <mares112358@gmail.com>
2026-02-20 19:57:25 -0600

    Scheduler: Blink as a task

--- 03-Scheduler_Coop/03-Scheduler_Coop.c
+++ 03-Scheduler_Coop/03-Scheduler_Coop.c
@@ -3,6 +3,7 @@
 #include "raspBerryPico.h"
 
 volatile uint32_t system_tick_ms = 0;
+uint32_t last_blink_time = 0;
 
 void __attribute__((used)) isr_irq0(void)
 {
@@ -18,6 +19,16 @@ int main()
 {
     stdio_init_all();
 
+    GPIO1->CTRL &= ~(0x1F);
+    GPIO1->CTRL |= (0x05);
+
+    PADS->IO[1] = (0x1 << 0x4) | 0x40;
+
+    SIO->GPIO_OE &= ~(1 << 1);
+    SIO->GPIO_OE |= (1 << 1);
+
+    SIO->GPIO_OUT_CLR = (1 << 1);
+
     TIMER_Handle_t TimerBlink;
     TimerBlink.pTimerx = TIMER0;
     TimerBlink.TIMER_Config.TIMER_Alarm = TIMER0_ALARM0;
@@ -27,6 +38,10 @@ int main()
     nvic_iser[0] |= (1 << 0); // Enable TIMER0 IRQ in NVIC
 
     while (true) {
-
+        uint32_t now = system_tick_ms;
+        if (now - last_blink_time >= 1000) {
+            SIO->GPIO_OUT ^= (1 << 1); // Toggle GPIO1
+            last_blink_time += 1000; // Schedule next toggle in 1 second
+        }
     }
 }

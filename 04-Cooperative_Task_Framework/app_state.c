1bc342c85dcba31d4aaea1837f83d699cd03bc14
Miguel Angel Sanchez Mares <mares112358@gmail.com>
2026-07-13 14:01:08 -0600

    Added entry/exit actions for the FSM

--- 04-Cooperative_Task_Framework/app_state.c
+++ 04-Cooperative_Task_Framework/app_state.c
@@ -1,31 +1,101 @@
 #include "app_state.h"
 #include "raspBerryPico.h"
 #include "soft_timer.h"
+#include "uart.h"
 
 /* Set to 1 so the initial IDLE state is reported at startup */
 uint8_t state_changed_g = 1u;
 stw_timer_t p_diagnostic_timer = {0};
 
-void app_next_state(app_state_t *p_app_state)
-{
-    SIO->GPIO_OUT_CLR = (1u << 1);
-    SIO->GPIO_OUT_CLR = (1u << 2);
+// void app_next_state(app_state_t *p_app_state)
+// {
+//     SIO->GPIO_OUT_CLR = (1u << 1);
+//     SIO->GPIO_OUT_CLR = (1u << 2);
 
+//     switch(*p_app_state)
+//     {
+//         case APP_STATE_IDLE:
+//             *p_app_state = APP_STATE_RUNNING;
+//             break;
+
+//         case APP_STATE_RUNNING:
+//             *p_app_state = APP_STATE_DIAGNOSTIC;
+//             sw_timer_start(&p_diagnostic_timer, 3000u); // Start a 3-second timer for diagnostic state
+//             break;
+
+//         case APP_STATE_DIAGNOSTIC:
+//         default:
+//             *p_app_state = APP_STATE_IDLE;
+//             break;
+//     }
+//     state_changed_g = 1u;
+// }
+
+void app_transition(app_state_t *p_app_state, app_state_t new_state)
+{
     switch(*p_app_state)
     {
         case APP_STATE_IDLE:
-            *p_app_state = APP_STATE_RUNNING;
+            app_exit_idle();
             break;
-
+            
         case APP_STATE_RUNNING:
-            *p_app_state = APP_STATE_DIAGNOSTIC;
-            sw_timer_start(&p_diagnostic_timer, 3000u); // Start a 3-second timer for diagnostic state
+            app_exit_running();
             break;
 
         case APP_STATE_DIAGNOSTIC:
         default:
-            *p_app_state = APP_STATE_IDLE;
+            app_exit_diagnostic();
             break;
     }
+
+    *p_app_state = new_state;
+
+    switch(*p_app_state)
+    {
+        case APP_STATE_IDLE:
+            app_enter_idle();
+            break;
+            
+        case APP_STATE_RUNNING:
+            app_enter_running();
+            break;
+
+        case APP_STATE_DIAGNOSTIC:
+        default:
+            app_enter_diagnostic();
+            break;
+    }
+
     state_changed_g = 1u;
+}
+
+void app_enter_idle(void)
+{
+    uart_write_string("Enter IDLE\r\n");
+}
+void app_enter_running(void)
+{
+    uart_write_string("Enter RUNNING\r\n");
+}
+void app_enter_diagnostic(void)
+{
+    uart_write_string("Enter DIAGNOSTIC\r\n");
+    sw_timer_start(&p_diagnostic_timer, 3000u); // Start a 3-second timer for diagnostic state
+}
+
+void app_exit_idle(void)
+{
+    uart_write_string("Exit IDLE\r\n");
+    SIO->GPIO_OUT_CLR = (1u << 1) | (1u << 2);
+}
+void app_exit_running(void)
+{
+    uart_write_string("Exit RUNNING\r\n");
+    SIO->GPIO_OUT_CLR = (1u << 1) | (1u << 2);
+}
+void app_exit_diagnostic(void)
+{
+    uart_write_string("Exit DIAGNOSTIC\r\n");
+    SIO->GPIO_OUT_CLR = (1u << 1) | (1u << 2);
 }
--- 04-Cooperative_Task_Framework/app_state.h
+++ 04-Cooperative_Task_Framework/app_state.h
@@ -12,5 +12,15 @@ typedef enum
     APP_STATE_COUNT
 } app_state_t;
 
-void app_next_state(app_state_t *p_app_state);
- 
+// void app_next_state(app_state_t *p_app_state);
+void app_transition(app_state_t *p_app_state, app_state_t new_state);
+
+/* Entry Actions */
+static void app_enter_idle(void);
+static void app_enter_running(void);
+static void app_enter_diagnostic(void);
+
+/* Exit Actions */
+static void app_exit_idle(void);
+static void app_exit_running(void);
+static void app_exit_diagnostic(void);
